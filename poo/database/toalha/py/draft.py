8894efb94d357f03f8df6ee0fdd0db8ef3b589a5
Lucas Sousa Oliveira <ls5105254@gmail.com>
2025-09-16 20:22:45 +0000

    criando toalha

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,9 @@
+print ("Minha toalha está no varal")
+
+class Towel:
+    def __init__(self, color: str, size: str):
+        self.color = "Blue"
+        self.size = "M"
+        self.witness = 0
+
+def __str__(self):
